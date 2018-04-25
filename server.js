210b7246de43f0af97982cb6ec5dffbe5b42709d
CuHTeTuk <cuhtetuk@CuHTeTuks-MacBook-Pro.local>
2018-04-24 19:59:59 -0500

    hmtroutes

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ var path = require("path");
 //express info
 //=============================================================
 var app = express();
-var PORT = process.env.PORT || 3333;
+var PORT = process.env.PORT || 3336;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+require('./app/routing/htmlRoutes.js')(app);
+//require('./app/routing/apiRoutes.js')(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
